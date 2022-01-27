11ba07452da5ebc532406a46bb46206bcbdaccf9
vitorozias <98392927+vitorozias@users.noreply.github.com>
2022-01-27 19:17:15 -0300

    beecrowd | 1016

---
+++ Distância - beecrowd 1016.py
@@ -0,0 +1,8 @@
+#pedindo a distancia dos dois carros, x e y
+distancia = int(input())
+
+#calculando em minutos (lembrando que a cada 2 min ele se afasta 1km)
+resultado = distancia * 2
+
+#mostrando o resultado sem numeros depois da virgula
+print ("%d minutos" %(resultado))
