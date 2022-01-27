1cc5b8477b5312105983b1763c4fbf573970cbf1
vitorozias <98392927+vitorozias@users.noreply.github.com>
2022-01-27 19:17:54 -0300

    beecrowd | 2483

---
+++ Feliz Natal Beecrowd 2483.py
@@ -0,0 +1,13 @@
+i = int(input())
+contador = 0
+contador2 = 0
+print ("Feliz nat", end="")
+while (1<i<=10**4) and (contador<i):
+    print("a", end="")
+    contador = contador+1
+else:
+    while (i<=1) and (contador2<1):
+        print ("a", end="")
+        contador2 = contador2 + 1
+    else:
+        print ("l!")
